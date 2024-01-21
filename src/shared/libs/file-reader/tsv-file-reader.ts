d8a2095753ff5815805e803f19f3fb1f695e8401
masechkacat <masechkacat@gmail.com>
2024-01-21 13:05:26 +0100

    потоки

--- src/shared/libs/file-reader/tsv-file-reader.ts
+++ src/shared/libs/file-reader/tsv-file-reader.ts
@@ -1,75 +1,36 @@
+import EventEmitter from 'node:events';
+import { createReadStream } from 'node:fs';
 import { FileReader } from './file-reader.interface.js';
-import { readFileSync } from 'node:fs';
-import { Offer } from '../../types/index.js';
-import { Cities, OfferType, Amenities, UserType, BooleanString } from '../../types/index.js';
 
-export class TSVFileReader implements FileReader {
-  private rawData = '';
+const CHUNK_SIZE = 16384; // 16KB
 
-  constructor(private readonly filename: string) {}
-
-  public read(): void {
-    this.rawData = readFileSync(this.filename, { encoding: 'utf-8' });
+export class TSVFileReader extends EventEmitter implements FileReader {
+  constructor(private readonly filename: string) {
+    super();
   }
 
-  public toArray(): Offer[] {
-    if (!this.rawData) {
-      throw new Error('File was not read');
+  public async read(): Promise<void> {
+    const readStream = createReadStream(this.filename, {
+      highWaterMark: CHUNK_SIZE,
+      encoding: 'utf-8',
+    });
+
+    let remainingData = '';
+    let nextLinePosition = -1;
+    let importedRowCount = 0;
+
+    for await (const chunk of readStream) {
+      remainingData += chunk.toString();
+
+      while ((nextLinePosition = remainingData.indexOf('\n')) >= 0) {
+        const completeRow = remainingData.slice(0, nextLinePosition + 1);
+        remainingData = remainingData.slice(++nextLinePosition);
+        importedRowCount++;
+
+        this.emit('line', completeRow);
+      }
     }
-    return this.rawData
-      .split('\n')
-      .filter((row) => row.trim().length > 0)
-      .map((line) => line.split('\t'))
-      .map(([
-        title,
-        description,
-        postDate,
-        city,
-        previewImage,
-        photos,
-        isPremium,
-        isFavorite,
-        rating,
-        type,
-        rooms,
-        guests,
-        cost,
-        amenities,
-        name,
-        email,
-        avatarPath,
-        userTypeString,
-        commentsCount,
-        coordinates
-      ]) => {
-        const [latitude, longitude] = coordinates ? coordinates.split(',').map(Number) : [];
-        const userType = userTypeString as keyof typeof UserType;
-        const amenitiesKeys = amenities ? amenities.split(',').map((amenity) => {
-          const key = amenity.trim() as keyof typeof Amenities;
-          if (!Amenities[key]) {
-            throw new Error(`Unknown amenity: ${amenity}`);
-          }
-          return key;
-        }) : [];
-        return ({
-          title,
-          description,
-          postDate: new Date(postDate),
-          city: Cities[city as keyof typeof Cities],
-          previewImage,
-          photos: photos ? photos.split(',') : [],
-          isPremium: isPremium === BooleanString.TRUE,
-          isFavorite: isFavorite === BooleanString.TRUE,
-          rating: Number.parseFloat(rating),
-          type: OfferType[type as keyof typeof OfferType],
-          amenities: amenitiesKeys as (keyof typeof Amenities)[],
-          rooms: Number.parseInt(rooms, 10),
-          guests: Number.parseInt(guests, 10),
-          cost: Number.parseInt(cost, 10),
-          author: { name, email, avatarPath, userType },
-          commentsCount: Number.parseInt(commentsCount, 10),
-          coordinates: { latitude, longitude }
-        });
-      });
+
+    this.emit('end', importedRowCount);
   }
 }
--- src/shared/types/user.type.ts
+++ src/shared/types/user.type.ts
@@ -4,7 +4,7 @@ export type User = {
     //id: string; // Уникальный идентификатор пользователя
     name: string; // Мин. 1 символ, макс. 15 символов
     email: string; // Валидный адрес электронной почты
-    avatarUrl?: string; // URL аватара (необязательно)
+    avatarPath?: string; // URL аватара (необязательно)
     //password: string; // Мин. 6 символов, макс. 12 символов
     userType: keyof typeof UserType; // Тип пользователя
 };
