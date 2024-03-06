6de8d789a55a1a5163e7af6add31192a71982056
masechkacat <masechkacat@gmail.com>
2024-03-06 13:27:33 +0100

    fix for check

--- src/shared/modules/offer/dto/create-offer.dto.ts
+++ src/shared/modules/offer/dto/create-offer.dto.ts
@@ -1,7 +1,8 @@
-import { IsString, IsEnum, IsBoolean, Min, Max, IsLatitude, IsLongitude, ValidateNested, IsInt, Length } from 'class-validator';
+import { IsString, IsEnum, IsBoolean, Min, Max, IsLatitude, IsLongitude, ValidateNested, IsInt, Length, MaxLength, ArrayMaxSize, ArrayMinSize, ArrayNotEmpty } from 'class-validator';
 import { Cities, OfferType, Amenities, EnumValues } from '../../../types/index.js';
 import { Type } from 'class-transformer';
 import { validationMessages } from './create-offer.validation-messages.js';
+import { TITLE_MIN_LENGTH, TITLE_MAX_LENGTH, DESCRIPTION_MIN_LENGTH, DESCRIPTION_MAX_LENGTH, ROOMS_MIN, ROOMS_MAX, GUESTS_MIN, GUESTS_MAX, COST_MIN, COST_MAX, PREVIEW_IMAGE_MAX_LENGTH, PHOTOS_MAX_LENGTH, PHOTOS_ARRAY_SIZE } from './const-validation.js';
 
 export class CoordinatesDto {
   @IsLatitude({ message: validationMessages.coordinates.latitude.isLatitude })
@@ -13,11 +14,11 @@ export class CoordinatesDto {
 
 export class CreateOfferDto {
   @IsString({ message: validationMessages.title.isString })
-  @Length(10, 100, { message: validationMessages.title.length })
+  @Length(TITLE_MIN_LENGTH, TITLE_MAX_LENGTH, { message: validationMessages.title.length })
     title: string;
 
   @IsString({ message: validationMessages.description.isString })
-  @Length(20, 1024, { message: validationMessages.description.length })
+  @Length(DESCRIPTION_MIN_LENGTH, DESCRIPTION_MAX_LENGTH, { message: validationMessages.description.length })
     description: string;
 
   @IsEnum(Cities, { message: validationMessages.city.isEnum })
@@ -26,30 +27,39 @@ export class CreateOfferDto {
   @IsBoolean({ message: validationMessages.isPremium.isBoolean })
     isPremium: boolean;
 
-  @IsEnum(OfferType, { message: validationMessages.type.isEnum })
-    type: EnumValues<typeof OfferType>;
+  @MaxLength(PREVIEW_IMAGE_MAX_LENGTH, { message: validationMessages.previewImage.maxLength })
+    previewImage: string;
 
-  @IsInt({ message: validationMessages.rooms.isInt })
-  @Min(1, { message: validationMessages.rooms.min })
-  @Max(8, { message: validationMessages.rooms.max })
-    rooms: number;
+  @MaxLength(PHOTOS_MAX_LENGTH, { each: true, message: validationMessages.photos.maxLength })
+  @ArrayMinSize(PHOTOS_ARRAY_SIZE, { message: validationMessages.photos.arrayMinSize })
+  @ArrayMaxSize(PHOTOS_ARRAY_SIZE, { message: validationMessages.photos.arrayMaxSize })
+  @ArrayNotEmpty({ message: validationMessages.photos.arrayNotEmpty })
+    photos: string[] = new Array(PHOTOS_ARRAY_SIZE).fill('');
 
-  @IsInt({ message: validationMessages.guests.isInt })
-  @Min(1, { message: validationMessages.guests.min })
-  @Max(10 , { message: validationMessages.guests.max })
-    guests: number;
+    @IsEnum(OfferType, { message: validationMessages.type.isEnum })
+      type: EnumValues<typeof OfferType>;
 
-  @IsInt({ message: validationMessages.cost.isInt })
-  @Min(100, { message: validationMessages.cost.min })
-  @Max(100000, { message: validationMessages.cost.max })
-    cost: number;
+    @IsInt({ message: validationMessages.rooms.isInt })
+    @Min(ROOMS_MIN, { message: validationMessages.rooms.min })
+    @Max(ROOMS_MAX, { message: validationMessages.rooms.max })
+      rooms: number;
 
-  @IsEnum(Amenities, { each: true , message: validationMessages.amenities.isEnum })
-    amenities: EnumValues<typeof Amenities>[];
+    @IsInt({ message: validationMessages.guests.isInt })
+    @Min(GUESTS_MIN, { message: validationMessages.guests.min })
+    @Max(GUESTS_MAX, { message: validationMessages.guests.max })
+      guests: number;
 
-  authorId: string;
+    @IsInt({ message: validationMessages.cost.isInt })
+    @Min(COST_MIN, { message: validationMessages.cost.min })
+    @Max(COST_MAX, { message: validationMessages.cost.max })
+      cost: number;
 
-  @ValidateNested()
-  @Type(() => CoordinatesDto)
-    coordinates: CoordinatesDto;
+    @IsEnum(Amenities, { each: true , message: validationMessages.amenities.isEnum })
+      amenities: EnumValues<typeof Amenities>[];
+
+    authorId: string;
+
+    @ValidateNested()
+    @Type(() => CoordinatesDto)
+      coordinates: CoordinatesDto;
 }
--- src/shared/modules/user/dto/create-user.dto.ts
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -2,16 +2,21 @@ import { UserType, EnumValues } from '../../../types/index.js';
 import { IsEmail, IsString, Length, IsEnum } from 'class-validator';
 import { userValidationMessages } from './create-user.validation-messages.js';
 
+const NAME_MIN_LENGTH = 1;
+const NAME_MAX_LENGTH = 15;
+const PASSWORD_MIN_LENGTH = 6;
+const PASSWORD_MAX_LENGTH = 12;
+
 export class CreateUserDto {
   @IsString({ message: userValidationMessages.name.isString })
-  @Length(1, 15, { message: userValidationMessages.name.length })
+  @Length(NAME_MIN_LENGTH, NAME_MAX_LENGTH, { message: userValidationMessages.name.length })
     name: string;
 
   @IsEmail({}, { message: userValidationMessages.email.isEmail })
     email: string;
 
   @IsString({ message: userValidationMessages.password.isString })
-  @Length(6, 12, { message: userValidationMessages.password.length })
+  @Length(PASSWORD_MIN_LENGTH, PASSWORD_MAX_LENGTH, { message: userValidationMessages.password.length })
     password: string;
 
   @IsEnum(UserType, { message: userValidationMessages.userType.isEnum })
